2cc989a37c7451651933585f8917d28b94a3c52b
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2024-07-20 14:12:43 +0530

    ti2_comm

--- 20230530_w2053430/w2053430_PlaneManagement/src/Ticket.java
+++ 20230530_w2053430/w2053430_PlaneManagement/src/Ticket.java
@@ -48,7 +48,30 @@ public class Ticket {
             return person;
         }
 
-   
+    public static void mainTXTprint(int rowNumber , int seatN ,String Information) {
+            String rowN_letter;
+        if ( rowNumber == 0 ){
+            rowN_letter = "A";
+        } else if ( rowNumber == 1 ) {
+            rowN_letter = "B";
+        } else if (rowNumber == 2 ) {
+            rowN_letter = "C";
+        } else {
+            rowN_letter ="D";
+        }
+
+        String txt ="TicketFiles\\"+rowN_letter+(seatN+1)+".txt";
+        try {
+            File saveFileName = new File(txt);
+            System.out.println("File created : " + saveFileName.getName());
+
+            FileWriter saveFile = new FileWriter(saveFileName);
+            saveFile.write(Information);
+            saveFile.close();
+
+        } catch (Exception e){
+            System.out.println("Error..");
+        }
     }
 }
 
