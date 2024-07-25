261feb68fa568919efdf1eaa1d8882ef25240440
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2024-07-26 00:25:51 +0530

    fainl

--- 20230530_w2053430/w2053430_PlaneManagement/src/Person.java
+++ 20230530_w2053430/w2053430_PlaneManagement/src/Person.java
@@ -9,6 +9,28 @@ public class Person {
         this.email = emailInput;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
