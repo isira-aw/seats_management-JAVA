8af72801c85c3e2c91c8621d759a76685e5bc065
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2024-07-26 00:25:16 +0530

    prson
    new one

---
+++ 20230530_w2053430/w2053430_PlaneManagement/src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String email;
+
+    public Person(String nameInput, String surnameInput, String emailInput) {
+        this.name = nameInput;
+        this.surname = surnameInput;
+        this.email = emailInput;
+    }
+
+
+
+}
